f13f3ef5495d2e69793d5e57c9c76190916f52b3
Luiz Henrique Santos de Andrade <71150787+Andradev101@users.noreply.github.com>
2021-08-15 05:44:44 +0000

    Passed test 2, still trying to figure out the test 3 CastError.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 
 const mySecret = process.env['MONGO_URI']
 const mongoose = require('mongoose')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const Schema = mongoose.Schema;
 
 app.use(bodyParser.urlencoded({extended: false}))
 require('dotenv').config()
@@ -19,6 +20,7 @@ mongoose.connect(mySecret, {
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -26,18 +28,30 @@ db.once('open', function() {
 });
 //db connected
 
-const newUserSchema = new mongoose.Schema({
-  username: String
-})//creating a schema
+//creating schemas
+const newUserSchema = new Schema({
+	//_id: String,
+  	username: String
+})
+//compiling it into models
+const User = mongoose.model('user', newUserSchema);
 
-const User = mongoose.model('User', newUserSchema);
-//compiling it into a model
+const newExerciseSchema = new Schema({
+	//_id: String,
+  	description: {type:String, required:true},
+  	duration: {type:Number, required:true},
+  	date: String,
+  	username: {type: Schema.Types.ObjectId, ref: 'user'},
+});
+
+//compiling it into models
+const Exercise = mongoose.model('exercise', newExerciseSchema);
 
 app.post('/api/users', (req, res) =>{
   const user = new User({username: req.body.username});
   //creating a new user with the request from the form on the body
   user.save(function (err, data) { //saving it [which assigns it an _id]
-    if (err) return handleError(err);//if error return error
+    if (err) return console.log(err);//if error return error
     res.json({//success
       username: data.username,
       _id: data._id
@@ -45,8 +59,64 @@ app.post('/api/users', (req, res) =>{
   });
 });
 //FUCK YEAH IT FUCKING WORKED IMA FEKIN GENIUS
-//HALLO
+app.get('/api/users', function (req, res) {
+  User.find({}).select({__v: 0}).exec((err, data) => {
+    if (err) console.error(err);
+    res.json(data)
+  })
+})
+//it was right from the beginning but i had wrong answers before, so i had to use User.deleteMany({}).exec((err, data) => {
+//     if (err) console.error(err);
+//   })
+//to delete all the old inputs
+
+//TO DO BELOW
+app.post('/api/users/:_id/exercises', (req, res) =>{
+	
+  if(req.body.date == ""){
+    var currentDate = new Date();
+    req.body.date = currentDate.toDateString()
+  }
+  	const exercise = new Exercise({
+		description: req.body.description,
+		duration: req.body.duration,
+		date: req.body.duration
+	});
+	
+/* 	exercise.save((err, data) => {
+		if (err) console.error(err);
+		res.json({
+			_id: data._id,
+			description: data.description,
+			duration: data.duration,
+			date: data.date
+		})
+  	}); */
+
+	var userId = '61188524f489c80284fe14cb';
+	console.log(userId);
+	
+	User.findByIdAndUpdate(userId)
+	.populate('username')
+	.exec((err, data) =>{
+		if (err) res.send(err)
+		console.log(data)
+	})
+})
+
+app.get('/clear', (req, res) =>{
+	User.findById('61188524f489c80284fe14cb', (err, data) =>{
+		res.json(data)
+	})
+	// User.deleteMany({}, function(err) { 
+	// 	console.log('collection removed') 
+	// });
+
+	// Exercise.deleteMany({}, function(err) { 
+	// 	console.log('collection removed') 
+	// });
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+	console.log('Your app is listening on port ' + listener.address().port)
 })
